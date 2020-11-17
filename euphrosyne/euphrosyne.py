702b657721befd6421802b57e5fe8dca4e28d78a
Montblac <Montblac@users.noreply.github.com>
2020-11-16 21:12:13 -0800

    Implement hourly time check

--- euphrosyne/euphrosyne.py
+++ euphrosyne/euphrosyne.py
@@ -1,16 +1,22 @@
-from tweet import Tweet
 from submission import Submission
+from tweet import Tweet
+from datetime import datetime
+from time import sleep
 
-try:
-    submission = Submission(['aww'])
-    submission.generate()
+while True:
+    time = datetime.now()
 
-    permalink = submission.permalink()
-    url = submission.url()
-    permalink_short = submission.permalink_short()
+    # Check if current time is within 2 minutes of the hour
+    if time.minute <= 2 or time.minute >= 58:
+        submission = Submission(['aww'])
+        submission.generate()
 
-    tweet = Tweet(permalink, url, permalink_short)
-    tweet.post()
+        permalink = submission.permalink()
+        url = submission.url()
+        permalink_short = submission.permalink_short()
 
-except SystemExit:
-    print()
+        tweet = Tweet(permalink, url, permalink_short)
+        tweet.post()
+
+    print('Trying again after {} minutes.\n'.format(60 - time.minute))
+    sleep(60 * (60 - time.minute))
